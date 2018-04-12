cdca80312380f19a485dfcfb6c58dd38e8451e84
xiaoyi001yeye <xiaoyi001@qq.com>
2018-04-12 11:26:30 +0800

    Increase the optimization of the native command execution, the native command does not use SSH.

--- xcall.sh
+++ xcall.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-
 params=$@
-i=201
-for (( i=201 ; i <= 204 ; i = $i + 1 )) ; do
-    echo ============= s$i $params =============
-    echo
-    ssh s$i "$params"
-    echo
+pcount=$#
+if (( $pcount < 1 )) ; then
+    echo no args;
+    exit ;
+fi
+for ((i=1 ; i<=5 ; i=$i+1 )) ; do
+    if [ "`hostname`" == "hdp00$i" ] ; then
+        echo ==========localhost hdp00$i $params========
+        "$params"
+    else
+        echo ============hdp00$i $params============
+        echo
+        ssh hdp00$i "$params"
+        echo
+
+    fi
 done
